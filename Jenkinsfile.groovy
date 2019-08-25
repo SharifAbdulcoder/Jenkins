33b759ba51078755b2d4af732c57a317a487ffcd
SharifAbdulCoder <abdugofir@gmail.com>
2019-08-25 14:39:35 -0500

    html

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,7 +3,12 @@ pipeline {
   stages {
     stage('Run Command') {
       steps {
-        sh "echo Hello, ... its mee"
+        sh '''
+        echo "Hello, ... its mee"
+        yum install httpd -y
+        ping -c 4 google.com
+        echo "My Jenknins worksss > /var/www/html/index.html"
+        '''
       }
     }
   }
