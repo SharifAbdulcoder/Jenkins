58ada69e1f64cece0c49e8e486093aca30c61338
SharifAbdulCoder <abdugofir@gmail.com>
2019-08-25 15:21:57 -0500

    :)

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -5,8 +5,8 @@ pipeline{
             steps{
                 sh '''
                 set +xe
-                echo "Hello, ... Its me"
-                ech  Error
+                echo "Hello"
+                ech  "Herro"
                 sudo yum install httpd wget unzip -y
                 ping -c 4 google.com
                 '''
@@ -19,8 +19,13 @@ pipeline{
                         def exists = fileExists 'terraform_0.12.7_linux_amd64.zip'
                         if (exists) {
                             sh "unzip -o terraform_0.12.7_linux_amd64.zip"
+                            sh "sudo mv terraform /bin"
+                            sh "terraform version"
                         } else {
                             sh "wget https://releases.hashicorp.com/terraform/0.12.7/terraform_0.12.7_linux_amd64.zip"
+                            sh "unzip -o terraform_0.12.7_linux_amd64.zip"
+                            sh "sudo mv terraform /bin"
+                            sh "terraform version"
                         }
                     }
                 }
