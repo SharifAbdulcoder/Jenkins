70b096f0fe77b49af2ff890173b2f000af54453b
SharifAbdulCoder <abdugofir@gmail.com>
2019-08-25 14:43:29 -0500

    miss

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -5,7 +5,7 @@ pipeline {
       steps {
         sh '''
         set +xe
-        echo "Hello, ... its mee"
+        ech "Hello, ... its mee"
         sudo yum install httpd -y
         ping -c 4 google.com
         echo "My Jenknins worksss > /var/www/html/index.html"
