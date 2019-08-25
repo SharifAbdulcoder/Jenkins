e5901ca00bde6f8b8e532080067dc80ea1d92c6a
SharifAbdulCoder <abdugofir@gmail.com>
2019-08-25 14:54:32 -0500

    terraform

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -20,6 +20,9 @@ pipeline {
       ws("tmp/")
       sh '''
       pwd
+      wget https://releases.hashicorp.com/terraform/0.12.7/terraform_0.12.7_linux_amd64.zip
+      unzip terraform_0.12.7_linux_amd64
+      sudo mv terraform /bin/
       '''
     }
 
