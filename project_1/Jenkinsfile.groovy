2b6b0a679921c7c4c7927a1e80b7bdf69eb17c43
SharifAbdulCoder <abdugofir@gmail.com>
2019-08-28 18:45:32 -0500

    properties added

--- project_1/Jenkinsfile.groovy
+++ project_1/Jenkinsfile.groovy
@@ -1,40 +1,41 @@
 node('master') {
-  stage('Clone repo') {
-    git 'https://github.com/SharifAbdulcoder/Jenkins.git'
-  }
+  properties([parameters([booleanParam(defaultValue: false, description: 'terraform apply', name: 'terraform-apply'), booleanParam(defaultValue: false, description: 'terraform destroy', name: 'terraform-destroy')])])
+    stage('Clone repo') {
+      git 'https://github.com/SharifAbdulcoder/Jenkins.git'
+    }
 
-  stage('Check terraform') {
-         try {
-           // Trying to run terraform command
-           env.terraform  = sh returnStdout: true, script: 'terraform --version'
-           echo """
-           echo terraform is already installed version ${env.terraform}
-           """
-           } catch(er) {
-                 // if terraform does not installed in system stage will install the terraform
-                  stage('Installing terraform') {
-                    sh """
-                    yum install terraform -y
-                    """
-                  }
+    stage('Check terraform') {
+           try {
+             // Trying to run terraform command
+             env.terraform  = sh returnStdout: true, script: 'terraform --version'
+             echo """
+             echo terraform is already installed version ${env.terraform}
+             """
+             } catch(er) {
+                   // if terraform does not installed in system stage will install the terraform
+                    stage('Installing terraform') {
+                      sh """
+                      yum install terraform -y
+                      """
+                    }
+                 }
                }
+
+
+    stage("Terraform plan"){
+             dir("${WORKSPACE}/project_1/") {
+             sh "terraform init"
+             sh "terraform plan"
              }
-
-
-  stage("Terraform plan"){
-           dir("${WORKSPACE}/project_1/") {
-           sh "terraform init"
-           sh "terraform plan"
            }
-         }
-         stage("Terraform apply"){
-           if (!params.terraform-destroy) {
-               if (params.terraform-apply) {
-                 dir("${WORKSPACE}/project_1/") {
-                   echo "#### Terraform Applying the Changes #####"
-                   sh "terraform apply --auto-approve"
+           stage("Terraform apply"){
+             if (!params.terraform-destroy) {
+                 if (params.terraform-apply) {
+                   dir("${WORKSPACE}/project_1/") {
+                     echo "#### Terraform Applying the Changes #####"
+                     sh "terraform apply --auto-approve"
+                   }
                  }
                }
              }
            }
-         }
