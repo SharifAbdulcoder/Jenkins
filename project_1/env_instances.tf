938f716a030099ef094745e5f6a34e4a272ba3cb
SharifAbdulCoder <abdugofir@gmail.com>
2019-08-28 18:56:15 -0500

    more }

--- project_1/env_instances.tf
+++ project_1/env_instances.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "Dev" {
   ami                         = "ami-0b898040803850657"
   instance_type               = "t2.micro"
-  key_name                    = "jenkins_key}"
+  key_name                    = "jenkins_key"
   subnet_id                   = "subnet-838061ce"
   associate_public_ip_address = "true"
 }
@@ -13,7 +13,7 @@ resource "aws_instance" "Dev" {
 resource "aws_instance" "QA" {
   ami                         = "ami-0b898040803850657"
   instance_type               = "t2.micro"
-  key_name                    = "jenkins_key}"
+  key_name                    = "jenkins_key"
   subnet_id                   = "subnet-b55b50ba"
   associate_public_ip_address = "true"
 }
@@ -21,7 +21,7 @@ resource "aws_instance" "QA" {
 resource "aws_instance" "PROD" {
   ami                         = "ami-0b898040803850657"
   instance_type               = "t2.micro"
-  key_name                    = "jenkins_key}"
+  key_name                    = "jenkins_key"
   subnet_id                   = "subnet-cd257791"
   associate_public_ip_address = "true"
 }
