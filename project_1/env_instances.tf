1e8169df4e539be042ec2c3ab8f3adfe4e4d4a94
SharifAbdulCoder <abdugofir@gmail.com>
2019-08-28 18:35:29 -0500

    terraform env

---
+++ project_1/env_instances.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  region = "${var.region}"
+}
+
+resource "aws_instance" "Dev" {
+  ami                         = "ami-02eac2c0129f6376b}"
+  instance_type               = "t2.micro"
+  key_name                    = "jenkins_key}"
+  subnet_id                   = "subnet-838061ce"
+  associate_public_ip_address = "true"
+}
+
+resource "aws_instance" "QA" {
+  ami                         = "ami-02eac2c0129f6376b}"
+  instance_type               = "t2.micro"
+  key_name                    = "jenkins_key}"
+  subnet_id                   = "subnet-b55b50ba"
+  associate_public_ip_address = "true"
+}
+
+resource "aws_instance" "PROD" {
+  ami                         = "ami-02eac2c0129f6376b}"
+  instance_type               = "t2.micro"
+  key_name                    = "jenkins_key}"
+  subnet_id                   = "subnet-cd257791"
+  associate_public_ip_address = "true"
+}
