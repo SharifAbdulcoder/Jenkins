8b086ec846c99d656660d12bd0cdf3a63f8fb904
SharifAbdulCoder <abdugofir@gmail.com>
2019-08-28 18:49:42 -0500

    no pram

--- project_1/Jenkinsfile.groovy
+++ project_1/Jenkinsfile.groovy
@@ -1,5 +1,5 @@
 node('master') {
-  properties([parameters([booleanParam(defaultValue: false, description: 'terraform apply', name: 'terraform-apply')])])
+  // properties([parameters([booleanParam(defaultValue: false, description: 'terraform apply', name: 'terraform-apply')])])
     stage('Clone repo') {
       git 'https://github.com/SharifAbdulcoder/Jenkins.git'
     }
