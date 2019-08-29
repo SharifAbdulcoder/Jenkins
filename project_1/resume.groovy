e9bb837e55d1974a2a5cb5e3b3cb45c5ecfa3105
SharifAbdulCoder <abdugofir@gmail.com>
2019-08-28 19:17:55 -0500

    back to old v.

--- project_1/resume.groovy
+++ project_1/resume.groovy
@@ -7,22 +7,8 @@ node{
         sh "ssh  ec2-user@${ENVIR} sudo yum install git python-pip3 -y"
     }
     stage("Pull Repo"){
-      try {
-         // Trying to run terraform command
-         env.repo  = sh returnStdout: true, script: 'ls  /home/ec2-user/Resume_app'
-         echo """
-         echo repo exists  ${env.terraform}
-         """
-         } catch(er) {
-               // if terraform does not installed in system stage will install the terraform
-                stage('pull repo') {
-                  sh """
-                  sh "ssh  ec2-user@${ENVIR} git clone https://github.com/SharifAbdulcoder/Resume_app.git"
-                  """
-                }
-             }
-           }
-
+        sh "ssh  ec2-user@${ENVIR} git clone https://github.com/SharifAbdulcoder/Resume_app.git"
+    }
     stage("Install Requirements"){
         //sh "virtualenv /tmp/venv"
         //sh ". /tmp/venv/bin/activate"
